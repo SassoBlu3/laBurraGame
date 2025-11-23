3980545cdd7ccea7acd104ff787e881664089593
Sasso Blu <sassoblu3@gmail.com>
2025-11-23 20:40:26 +0100

    creacion de IDeck y clase Deck

---
+++ src/classes/Deck.ts
@@ -0,0 +1,31 @@
+import {IDeck} from "../interfaces/IDeck.js";//importar interfaz
+import { Card } from "./Card.js"; //importar clase
+
+export class Deck implements IDeck {
+    cards: Card[] = []; //inicializar array de cartas
+
+    constructor() {
+        this.initializeDeck();
+    }
+    private initializeDeck(): void {
+        const suits: string[] = ["♥", "♦", "♣", "♠"];
+        const values: string[] = [
+            "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
+        ];  
+        for (const suit of suits) { //recorrer palos
+            for (const value of values) { //recorrer valores
+                this.cards.push(new Card(suit, value)); //crear carta y añadir al array
+            }
+        }   
+    }
+
+    //crear metodo para barajar
+    public shuffle(): void {
+        this.cards.sort(() => Math.random() - 0.5);
+    }
+
+    //crear metodo para repartir 
+    public get dealCard(): Card | undefined {
+        return this.cards.pop(); //sacar ultima carta del array, la que est[a encima
+    }
+}
--- src/interfaces/IDeck.ts
+++ src/interfaces/IDeck.ts
@@ -1,1 +1,10 @@
+import { Card } from "../classes/Card.js"; 
+
+
 export interface IDeck {
+    cards: Card[]; // cartas en el mazo
+    shuffle(): void; // Método para barajar las cartas.
+    dealCard: Card | undefined; // Método para repartir una carta.
+
+}
+
