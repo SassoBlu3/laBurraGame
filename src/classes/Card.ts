801acaa80c4bab79ddaa143b620278f090facc1a
Sasso Blu <sassoblu3@gmail.com>
2025-11-26 19:33:44 +0100

    correccion de clase e Interfaz Card para agregar la Url de la API (imagenes)

--- src/classes/Card.ts
+++ src/classes/Card.ts
@@ -3,12 +3,13 @@ export class Card implements ICard {
     suit: string;
     value: string;
     rank: number;
+    imageUrl: string;
 
     constructor(suit: string, value: string) {
         this.suit = suit;
         this.value = value;
         this.rank = this.calcularRank(this.value);    
-    
+        this.imageUrl = this.generarUrlImage(suit, value);
     }
     
     public mostrarNombreCompleto(): string {
@@ -24,5 +25,23 @@ export class Card implements ICard {
             // Si no es letra, convertimos el texto "10", "9" a número real.
             default: return parseInt(valorVisual); 
         }
+    }
+    private generarUrlImage(suit: string, value: string): string {
+        let codigoPalo = "";
+        switch (suit) {
+            case "♥": codigoPalo = "H"; break; //corazones
+            case "♦": codigoPalo = "D"; break; //diamantes
+            case "♣": codigoPalo = "C"; break; //treboles
+            case "♠": codigoPalo = "S"; break;//espadas
+            default: codigoPalo = "H"; // Desconocido
+        }
+    // La API usa 0 para el 10
+    let codigoValor = value;
+    if (value === "10") {
+        codigoValor = "0"; 
+    }
+
+    //unir y crear url final
+    return `https://deckofcardsapi.com/static/img/${codigoValor}${codigoPalo}.png`
     }
 }
--- src/interfaces/ICard.ts
+++ src/interfaces/ICard.ts
@@ -2,4 +2,5 @@ export interface ICard {
     suit: string;  // 'suit' (palo) debe ser un texto (string), ej: "♥", "♠"
     value: string; // 'value' (valor) debe ser un texto, ej: "A", "10", "K"
     rank: number;  // El valor numérico real (13, 7)
+    imageUrl: string;// API de https://deckofcardsapi.com/ para la imagen de la carta
 }
